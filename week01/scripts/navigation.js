442bc53f9cb5a665eeafd444ee46f31d22e6abc0
Eugenia Schulz <eschulz@byupathway.edu>
2026-07-01 11:03:18 -0600

    week 01

---
+++ week01/scripts/navigation.js
@@ -0,0 +1,6 @@
+const menuButton = document.querySelector("#menu-button");
+const navMenu = document.querySelector("#nav-menu");
+
+menuButton.addEventListener("click", () => {
+  navMenu.classList.toggle("open");
+});
